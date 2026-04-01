19af7fa9dbefcb92afc23e7c01568694e9ef52d7
ana_mantella <146769187+anaclaramantellaa@users.noreply.github.com>
2026-04-01 19:27:18 -0300

    Add files via upload

---
+++ front-main2/aula10front/tabuada.js
@@ -0,0 +1,22 @@
+function construirTabela(){
+    let tabuada = document.querySelector("#tabuada tbody");
+    let valorA =  parseInt(document.querySelector('#valorA').value);
+
+    tabuada.innerHTML = "";
+
+    for (let valorB = 1; valorB<=10; valorB++){
+        let resultado = valorA * valorB;
+
+        let conteudo =  `<td> ${valorA} </td>
+                        <td> X </td>
+                        <td> ${valorB}</td>
+                        <td> = </td>
+                        <td> ${resultado} </td>`
+
+        let linha = document.createElement('tr');
+        linha.innerHTML = conteudo;
+        tabuada.append(linha);
+    }
+}
+
+document.querySelector(".btn").addEventListener('click', construirTabela)
